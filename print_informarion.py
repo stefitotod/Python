873dcdf9aff5b4884b24aa25733aa05060413a81
stefitotod <121817763+stefitotod@users.noreply.github.com>
2023-06-19 18:27:39 +0300

    print_information.py

---
+++ print_informarion.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+city = input("Enter your city name: ")
+age = int(input("Enter your age: "))
+a = "Hello"
+b = "you are from"
+c = "you are"
+print(f"{a} {name}, {b} {city}, {c} {age}!")
