32447b56df52bab746a37990bd8bfbbb7626dd3d
Stefani Todorova <121817763+stefitotod@users.noreply.github.com>
2023-06-20 16:47:02 +0300

    Update min_max_average_in_a_list.py

--- min_max_average_in_a_list.py
+++ min_max_average_in_a_list.py
@@ -1,7 +1,9 @@
 l = []
 length = int(input("Enter length: "))
+
 for i in range(length):
     num = int(input("Enter a number: "))
     l.append(num)
+
 print(f"min: {min(l)}, max: {max(l)}, average: {sum(l)/len(l)}")
 
